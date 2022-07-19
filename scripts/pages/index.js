8bea876bacffbc22d49b9ae72a5e1f9cbc75f206
Claire_M <clairerodriguez79@yahoo.fr>
2022-07-19 20:40:48 +0200

    Retrait fonction en commentaires

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -589,65 +589,6 @@ return selectedRecipesBySearch;
 }
 
 
-// function allRecipesFilter(filterSearchRecipes){
-//     let selectedRecipesBySearch = [];
-//     console.log("Fonction avec les recettes");
-//     filterSearchRecipes.filter((recipe)=>{
-//         if(
-//             recipe.name
-//             .toLowerCase()
-//             .replace(/\s/g, "")
-//             .includes(SearchBarValue)||
-//             recipe.description
-//             .toLowerCase()
-//             .replace(/\s/g, "")
-//             .includes(SearchBarValue)||
-//             recipe.ingredients.find((item) =>
-//                 item.ingredient
-//                 .toLowerCase()
-//                 .replace(/\s/g, "")
-//                 .includes(SearchBarValue)
-//             )
-
-//         ){
-//         selectedRecipesBySearch.push(recipe);
-//         selectedRecipesBySearch = [...new Set(selectedRecipesBySearch)];
-//         // console.log("Tableau selectedRecipesBySearch",selectedRecipesBySearch);
-
-//         }
-//     })
-//     for (let recipe of filterSearchRecipes) {
-//         console.log("Recipe dans le for",recipe);
-//         if (
-//             recipe.name
-//             .toLowerCase()
-//             .replace(/\s/g, "")
-//             .includes(SearchBarValue)||
-//             recipe.description
-//             .toLowerCase()
-//             .replace(/\s/g, "")
-//             .includes(SearchBarValue))
-//     {
-//         selectedRecipesBySearch.push(recipe);
-//         selectedRecipesBySearch = [...new Set(selectedRecipesBySearch)];
-//         // console.log("Tableau selectedRecipesBySearch",selectedRecipesBySearch);
-//     }
-//     for (let i = 0; i < recipe.ingredients.length; i++) {
-//         const ingredientName = recipe.ingredients[i].ingredient
-//             .toLowerCase()
-//             .replace(/\s/g, "");
-//         //console.log(ingredientName);
-//         if (ingredientName.includes(SearchBarValue)) {
-//             selectedRecipesBySearch.push(recipe);
-//             selectedRecipesBySearch = [...new Set(selectedRecipesBySearch)];
-//         }
-//     }  
-// }
-// return selectedRecipesBySearch;
-// }
-
-
-
 //2
 //Fonction ds le listener de l'input
 function principalFilter(e){
